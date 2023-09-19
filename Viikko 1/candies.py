c0faafce4b7d4ce6f06e8cbdcbd20cb35a932284
laivii <91199307+laivii@users.noreply.github.com>
2023-09-19 13:51:14 +0300

    tehtävät

---
+++ Viikko 1/candies.py
@@ -0,0 +1,14 @@
+def count(a, b, c):
+    nallekarkki = a
+    suklaakarkki = b
+    rahaa = c
+
+    nalleja = rahaa / nallekarkki
+    suklaata = rahaa / suklaakarkki
+    
+
+if __name__ == "__main__":
+    print(count(3, 4, 11)) # 3
+    #print(count(5, 1, 100)) # 100
+    #print(count(2, 3, 1)) # 0
+    #print(count(2, 3, 9)) # 4
