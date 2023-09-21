8980f00bf201af3e944bb62904455e9baca3b8a3
laivii <91199307+laivii@users.noreply.github.com>
2023-09-21 12:12:49 +0300

    Eri summat

---
+++ Viikko 2/sumdiff.py
@@ -0,0 +1,13 @@
+def create(n):
+    määrä = n
+    lukujonot = []
+    
+    for i in range(1, n+1):
+        lukujonot.append(i)
+    
+    return lukujonot
+
+if __name__ == "__main__":
+    print(create(6)) # [3, 1, 6, 2, 4, 5]
+    print(create(10)) # [7, 10, 3, 1, 5, 4, 8, 6, 9, 2]
+    print(create(15)) # [9, 4, 6, 14, 15, 13, 12, 11, 5, 2, 3, 8, 1, 7, 10]
